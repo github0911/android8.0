d2934ecf880e37c4735bf01ec792921a0fc53bd1
justin <justin.m.m.zhang@gmail.com>
2018-09-01 12:28:51 +0800

    加入日志打印框架

--- app/src/main/java/com/example/android80/MainActivity.java
+++ app/src/main/java/com/example/android80/MainActivity.java
@@ -27,16 +27,33 @@ import com.example.android80.api.MovieService;
 import com.example.android80.entity.MovieEntity;
 import com.google.gson.Gson;
 import com.google.zxing.client.android.encode.QRCodeEncoder;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import java.util.ArrayList;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private static NotificationManagerCompat notificationManager;
     String channelId = "channelId";
@@ -48,6 +65,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(true)  // (Optional) Whether to show thread info or not. Default true
+                .methodCount(0)         // (Optional) How many method line to show. Default 2
+                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+                .tag(TAG)   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build();
+
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.tv_send);
         textView.setOnClickListener(this);
@@ -145,27 +170,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //https://gank.io/post/56e80c2c677659311bed9841
         String baseUrl = "https://api.douban.com/v2/movie/";
 
+        //addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        //添加Retrofit对Rxjava 返回的就是一个Observable
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
         MovieService service = retrofit.create(MovieService.class);
 
-        Call call = service.getTopMovie(0, 1);
-        call.enqueue(new Callback<MovieEntity>() {
-            @Override
-            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
-                MovieEntity entity = response.body();
-                textView.setText(new Gson().toJson(entity.getTitle()));
-            }
+        service.getTopMovie(0, 1)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<MovieEntity>() {
+                    Disposable disposable;
 
-            @Override
-            public void onFailure(Call<MovieEntity> call, Throwable t) {
-                textView.setText(t.getMessage());
-            }
-        });
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable  = d;
+                        if (!d.isDisposed()) {
+                            Logger.d("method %s", "onSubscribe");
+                        }
+                    }
 
+                    @Override
+                    public void onNext(MovieEntity movieEntity) {
+                        toast(movieEntity.getTitle());
+                        textView.setText(new Gson().toJson(movieEntity.getTitle()));
+                        Logger.d("method %s", "onNext");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        toast(e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                        if (disposable != null && !disposable.isDisposed()) {
+                            disposable.dispose();
+                        }
+                        Logger.d("method %s", "onComplete");
+                    }
+                });
+//        call.enqueue(new Callback<MovieEntity>() {
+//            @Override
+//            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
+//                MovieEntity entity = response.body();
+//                textView.setText(new Gson().toJson(entity.getTitle()));
+//            }
+//
+//            @Override
+//            public void onFailure(Call<MovieEntity> call, Throwable t) {
+//                textView.setText(t.getMessage());
+//            }
+//        });
+
+    }
+
+    private void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/example/android80/api/MovieService.java
+++ app/src/main/java/com/example/android80/api/MovieService.java
@@ -2,6 +2,7 @@ package com.example.android80.api;
 
 import com.example.android80.entity.MovieEntity;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -9,5 +10,5 @@ import retrofit2.http.Query;
 public interface MovieService {
 
     @GET("top250")
-    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+    Observable<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
 }
